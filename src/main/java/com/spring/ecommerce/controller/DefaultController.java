0de292631d6bdd78613b4d0fb61d6798ef7b2aa2
FahadHasan12345 <fhasan451@gmail.com>
2021-02-09 04:05:13 +0600

    hi

---
+++ src/main/java/com/spring/ecommerce/controller/DefaultController.java
@@ -0,0 +1,14 @@
+package com.spring.ecommerce.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+
+public class DefaultController {
+	
+	    public String g() {
+	    	return "HIIIIIIII";
+	    }
+}
